7b60f184561d66373b742629b1dec6fc8b2642c5
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-10-08 14:50:02 +0300

    A bit of hero section

---
+++ app/Pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../Components/Hero/index";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+};
+
+export default HomePage;
