2c020ff506fc7fce03f2e8e92af40c9e5ebed0ee
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-10-14 17:25:13 +0300

    Services section done

---
+++ app/Components/Services/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <section className="bg-gradient-to-br from-green-400 to-lime-600 py-20 px-16 flex flex-col justify-center items-center gap-16 text-white shadow-lg">
+      <div className="flex flex-col gap-8 items-center">
+        <div className="font-bold text-4xl md:text-5xl">Paslaugos</div>
+        <p className="text-sm md:text-lg text-center text-white w-full md:w-4/5">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas aliquid
+          atque distinctio, perspiciatis recusandae aperiam. Earum blanditiis
+          doloribus tempora corporis odio sit odit consectetur libero. Dolorem
+          ducimus magnam nobis molestiae recusandae tempore impedit aperiam ea
+          corrupti asperiores autem modi assumenda repudiandae, velit possimus,
+          obcaecati, eos nemo eaque voluptatum.
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row gap-4">
+        <button className="bg-gradient-to-br from-green-500 to-lime-900 text-white font-bold py-2 px-6 md:py-4 md:px-8 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transform transition duration-300 ease-in-out hover:scale-105">
+          Registracija
+        </button>
+        <button className="bg-gradient-to-br from-green-500 to-lime-900 text-white font-bold py-2 px-6 md:py-4 md:px-8 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transform transition duration-300 ease-in-out hover:scale-105">
+          Tvarkaraštis
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
