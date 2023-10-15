e4bf5412d36e4366ee7a1989ca2ae8d5ee9ce29d
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-10-15 20:30:29 +0300

    A bit of hero

--- app/Components/Hero/index.tsx
+++ app/Components/Hero/index.tsx
@@ -3,18 +3,19 @@ import Image from "next/image";
 
 const Main = () => {
   return (
-    <section className="min-h-screen mt-0 mb-auto pb-7 overflow-hidden flex flex-col bg-zinc-300 items-center justify-center">
+    <section className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-gray-100 to-gray-400 text-white">
       <div className="flex flex-col items-center">
-        <div className="mb-25 mb-10">
+        <div className="mb-10">
           <Image
             src="/Images/Logo/logo.png"
             alt="Yogi.Frogi.Logo"
-            width={700}
-            height={700}
+            width={550}
+            height={550}
+            className="animate-fade-down animate-duration-1000 ease-in-out"
           />
         </div>
-        <div>
-          <button className="bg-gradient-to-br from-green-500 to-lime-900 text-white font-bold py-2 px-6 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transform transition duration-300 ease-in-out hover:scale-105 shadow-md md:py-4 md:px-8 md:text-lg">
+        <div className="animate-jump-in animate-duration-500 animate-delay-1000">
+          <button className="bg-gradient-to-br from-green-400 to-lime-600 text-white font-bold text-lg py-2 px-6 md:py-3 md:px-7 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transform transition duration-300 ease-in-out hover:scale-105 shadow-xl">
             Daugiau
           </button>
         </div>
--- app/Components/Services/index.tsx
+++ app/Components/Services/index.tsx
@@ -18,7 +18,7 @@ const Services = () => {
         <button className="bg-gradient-to-br from-green-500 to-lime-900 text-white font-bold py-2 px-6 md:py-4 md:px-8 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transform transition duration-300 ease-in-out hover:scale-105">
           Registracija
         </button>
-        <button className="bg-gradient-to-br from-green-500 to-lime-900 text-white font-bold py-2 px-6 md:py-4 md:px-8 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transform transition duration-300 ease-in-out hover:scale-105">
+        <button className="bg-gradient-to-br from-green-400 to-lime-500 text-white font-bold py-2 px-6 md:py-4 md:px-8 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transform transition duration-300 ease-in-out hover:scale-105">
           Tvarkaraštis
         </button>
       </div>
