52f31d4f8867b468f2324dc1099eb7e07ff05f2b
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-11-18 21:04:19 +0200

    Reorganized project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import HomePage from "./Pages/HomePage";
-import RegPage from "./Pages/RegPage";
+import AboutMe from "@/Components/AboutMe";
+import AboutYoga from "@/Components/AboutYoga";
+import ContactsFooter from "@/Components/ContactsFooter";
+import Frog from "@/Components/Frog";
+import Main from "@/Components/Hero";
+import Services from "@/Components/Services";
 
 export default function Home() {
   return (
     <main>
-      <HomePage />
-      {/* <RegPage /> */}
+      <Main />
+      <div className="bg-gradient-to-br from-green-400 to-lime-600">
+        <AboutMe />
+        <AboutYoga />
+        <Services />
+      </div>
+      <ContactsFooter />
+      <Frog />
     </main>
   );
 }
---
+++ app/registracija/page.tsx
@@ -0,0 +1,15 @@
+import ContactsFooter from "@/Components/ContactsFooter";
+import Frog from "@/Components/Frog";
+import Reg from "@/Components/Registration/Reg";
+
+const RegPage = () => {
+  return (
+    <div>
+      <Reg />
+      <Frog />
+      <ContactsFooter />
+    </div>
+  );
+};
+
+export default RegPage;
