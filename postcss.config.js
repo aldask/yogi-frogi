b3ac2168fbd7b6b4d5f6636c0b9c301a0d6f06cb
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-11-20 19:37:28 +0200

    still figuring it out

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  content: [
-    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
